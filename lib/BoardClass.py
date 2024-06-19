bc705aaed2bcb9bd858080d91426792629af4eb3
ryanmutula <162963081+ryanmutula@users.noreply.github.com>
2024-06-19 21:33:03 +0300

    Update BoardClass.py

--- lib/BoardClass.py
+++ lib/BoardClass.py
@@ -44,28 +44,28 @@ class Board:
     def add_premium_squares(self):
         """
          Adds all of the premium squares that influence the word's score.
-         TWS- Triple Word Score
-         DWS- Double Word Score
-         TLS- Triple Letter Score
-         DLS- Double Letter Score
+         TW- Triple Word Score
+         DW- Double Word Score
+         TL- Triple Letter Score
+         DL- Double Letter Score
         """
         premium_squares = {
-            (0, 0): "TWS", (7, 0): "TWS", (14, 0): "TWS",
-            (0, 7): "TWS", (14, 7): "TWS", (0, 14): "TWS",
-            (7, 14): "TWS", (14, 14): "TWS",
-            (1, 1): "DWS", (2, 2): "DWS", (3, 3): "DWS", (4, 4): "DWS",
-            (1, 13): "DWS", (2, 12): "DWS", (3, 11): "DWS", (4, 10): "DWS",
-            (13, 1): "DWS", (12, 2): "DWS", (11, 3): "DWS", (10, 4): "DWS",
-            (13, 13): "DWS", (12, 12): "DWS", (11, 11): "DWS", (10, 10): "DWS",
-            (1, 5): "TLS", (1, 9): "TLS", (5, 1): "TLS", (5, 5): "TLS",
-            (5, 9): "TLS", (5, 13): "TLS", (9, 1): "TLS", (9, 5): "TLS",
-            (9, 9): "TLS", (9, 13): "TLS", (13, 5): "TLS", (13, 9): "TLS",
-            (0, 3): "DLS", (0, 11): "DLS", (2, 6): "DLS", (2, 8): "DLS",
-            (3, 0): "DLS", (3, 7): "DLS", (3, 14): "DLS", (6, 2): "DLS",
-            (6, 6): "DLS", (6, 8): "DLS", (6, 12): "DLS", (7, 3): "DLS",
-            (7, 11): "DLS", (8, 2): "DLS", (8, 6): "DLS", (8, 8): "DLS",
-            (8, 12): "DLS", (11, 0): "DLS", (11, 7): "DLS", (11, 14): "DLS",
-            (12, 6): "DLS", (12, 8): "DLS", (14, 3): "DLS", (14, 11): "DLS"
+            (0, 0): "TW", (7, 0): "TW", (14, 0): "TW",
+            (0, 7): "TW", (14, 7): "TW", (0, 14): "TW",
+            (7, 14): "TW", (14, 14): "TW",
+            (1, 1): "DW", (2, 2): "DW", (3, 3): "DW", (4, 4): "DW",
+            (1, 13): "DW", (2, 12): "DW", (3, 11): "DW", (4, 10): "DW",
+            (13, 1): "DW", (12, 2): "DW", (11, 3): "DW", (10, 4): "DW",
+            (13, 13): "DW", (12, 12): "DW", (11, 11): "DW", (10, 10): "DW",
+            (1, 5): "TL", (1, 9): "TL", (5, 1): "TL", (5, 5): "TL",
+            (5, 9): "TL", (5, 13): "TL", (9, 1): "TL", (9, 5): "TL",
+            (9, 9): "TL", (9, 13): "TL", (13, 5): "TL", (13, 9): "TL",
+            (0, 3): "DL", (0, 11): "DL", (2, 6): "DL", (2, 8): "DL",
+            (3, 0): "DL", (3, 7): "DL", (3, 14): "DL", (6, 2): "DL",
+            (6, 6): "DL", (6, 8): "DL", (6, 12): "DL", (7, 3): "DL",
+            (7, 11): "DL", (8, 2): "DL", (8, 6): "DL", (8, 8): "DL",
+            (8, 12): "DL", (11, 0): "DL", (11, 7): "DL", (11, 14): "DL",
+            (12, 6): "DL", (12, 8): "DL", (14, 3): "DL", (14, 11): "DL"
         }
         for (x, y), value in premium_squares.items():
             self.board[x][y] = f" {value} "
@@ -77,11 +77,11 @@ class Board:
 
         if first_turn:
             if direction == "right":
-                if not (y <= 7 < y + len(word)):
-                    return False
-            elif direction == "down":
-                if not (x <= 7 < x + len(word)):
-                    return False
+                if not (x == 7 and 7 >= y and 7 < y + len(word)):
+                   return False
+        elif direction == "down":
+            if not (y == 7 and 7 >= x and 7 < x + len(word)):
+                return False
 
         for i, letter in enumerate(word):
             if direction == "right":
