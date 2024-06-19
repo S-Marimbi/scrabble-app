0f6f157ba45eb470334f719d84ad96df923101ed
S-Marimbi <161571671+S-Marimbi@users.noreply.github.com>
2024-06-19 22:17:28 +0300

    Delete lib/gameloop.py

--- lib/gameloop.py
+++
@@ -1,9 +0,0 @@
-#initializing the game
-#initializing the board, tile bag and players
-#function to start the game
-def start_game():
-    board = initialize_board()
-    tile_bag = initialize_tile_bag()
-    players = [Player('Human', tile_bag), Player('Computer', tile_bag)]
-    current_player = 0
-    return board, tile_bag, players, current_player          
